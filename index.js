6067ea7cc07d781d5a3351468261bfc3b9792fde
Abdulrhmann21 <Abdulrhmann163@gmail.com>
2026-03-16 08:30:44 +0300

    change information

--- index.js
+++ index.js
@@ -4,23 +4,22 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
-}
+return a + b;      }
 
 /**
  * @param {string} str The string to reverse.
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
-}
+return str.split("").reverse().join("");}
 
 /**
  * @param {number[]} numbers An array of numbers.
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+    if (numbers.length === 0) return null;
+    return Math.max(numbers);
 }
 
 /**
@@ -30,16 +29,15 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
-}
+  const cleaned = str.toLowerCase();
+  return cleaned === cleaned.split("").reverse().join("");}
 
 /**
  * @param {number[]} numbers An array of numbers.
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
-}
+return numbers.filter(num => num % 2 === 0);}
 
 
 // Do not edit the line below.
